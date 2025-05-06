76fa0807064b17a3619ed9ed30af3445056912d6
Sajat <73510259+realsajat@users.noreply.github.com>
2025-05-06 22:46:15 +0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/barkia-qatar-shop-online">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Products />} />
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/barkia-qatar-shop-online/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
